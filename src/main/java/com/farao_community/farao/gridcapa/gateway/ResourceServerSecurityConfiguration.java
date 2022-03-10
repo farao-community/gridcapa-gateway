158a6bf0642f253d1fa5e8c66e75a2bcf094a43f
benrejebmoh <mohamed.ben-rejeb@rte-france.com>
2022-03-10 09:48:14 +0100

    unsing gateway as an oauth2 resource server

---
+++ src/main/java/com/farao_community/farao/gridcapa/gateway/ResourceServerSecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.farao_community.farao.gridcapa.gateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+public class ResourceServerSecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+            .authorizeExchange()
+            .anyExchange().authenticated()
+            .and()
+            .oauth2ResourceServer()
+            .jwt();
+        return http.build();
+    }
+}
