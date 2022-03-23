938683848710ae82246e9df4ffaf8fa2b374d9af
Sébastien Murgey <sebastien.murgey@rte-france.com>
2022-03-23 11:04:11 +0100

    Add bearer token converter for websocket authentication

--- src/main/java/com/farao_community/farao/gridcapa/gateway/ResourceServerSecurityConfiguration.java
+++ src/main/java/com/farao_community/farao/gridcapa/gateway/ResourceServerSecurityConfiguration.java
@@ -3,7 +3,9 @@ package com.farao_community.farao.gridcapa.gateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.server.resource.web.server.ServerBearerTokenAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
 
 @Configuration
 public class ResourceServerSecurityConfiguration {
@@ -14,8 +16,13 @@ public class ResourceServerSecurityConfiguration {
             .authorizeExchange()
             .anyExchange().authenticated()
             .and()
-            .oauth2ResourceServer()
-            .jwt();
+            .oauth2ResourceServer(oauth2 -> oauth2.bearerTokenConverter(bearerTokenConverter()).jwt());
         return http.build();
+    }
+
+    ServerAuthenticationConverter bearerTokenConverter() {
+        ServerBearerTokenAuthenticationConverter bearerTokenConverter = new ServerBearerTokenAuthenticationConverter();
+        bearerTokenConverter.setAllowUriQueryParameter(true);
+        return bearerTokenConverter;
     }
 }
